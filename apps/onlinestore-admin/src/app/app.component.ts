9d6592c008b2648504058188a022262648085035
Rupesh Tiwari <rtiwari@strasz.com>
2020-11-24 17:52:00 -0500

    updated imports

--- apps/onlinestore-admin/src/app/app.component.ts
+++ apps/onlinestore-admin/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SalesProductsModule } from '@myorg/sales/products';
+import { SalesProductsModule } from '@myorg/sales-products';
 
 
 @Component({
