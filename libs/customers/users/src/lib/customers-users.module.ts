7822ec014a131affbbf57ec023b234cb701a5349
Rupesh Tiwari <rtiwari@strasz.com>
2020-11-30 11:05:54 -0500

    fixing that import issues

--- libs/customers/users/src/lib/customers-users.module.ts
+++ libs/customers/users/src/lib/customers-users.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {SalesProductEditorModule} from '@myorg/sales-product-editor'
 
 @NgModule({
   imports: [CommonModule],
 })
 export class CustomersUsersModule {
-  x = new SalesProductEditorModule();
+
 }
