bf755671e75b62278d235a0bec08f0e2cfb85e9d
Rupesh Tiwari <rtiwari@strasz.com>
2021-01-11 17:12:30 -0500

    removed un necessary import

--- libs/sales/products/src/lib/sales-products.module.ts
+++ libs/sales/products/src/lib/sales-products.module.ts
@@ -5,11 +5,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 // sales project can depend on branding's logger project
 import {BrandingLoggerModule} from '@myorg/branding/logger';
 
-// Sales project can not depend on Customers project
-import {CustomersUserEditorModule} from '@myorg/customers-user-editor';
 
-// Sales project can depend on other sales project
-import {SalesProductEditorModule} from '@myorg/sales-product-editor';
 
 @NgModule({
   imports: [CommonModule,BrandingLoggerModule],
