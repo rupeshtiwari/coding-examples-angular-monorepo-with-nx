f7bef7f043dcd95db67723c4c5d190ec490eae6e
rupeshtiwari <roopkt@gmail.com>
2021-01-19 16:15:44 -0500

    fix: fixing build error

--- libs/sales/products/src/lib/sales-products.module.ts
+++ libs/sales/products/src/lib/sales-products.module.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list/product-list.component';
 
-// sales project can depend on branding's logger project
-import {BrandingLoggerModule} from '@myorg/branding/logger';
+
 
 
 
 @NgModule({
-  imports: [CommonModule,BrandingLoggerModule],
+  imports: [CommonModule],
   declarations: [ProductListComponent],
   exports: [ProductListComponent],
 })
